1919e103f085cf601f590106de5eebe883f0038d
Richard Potter <richardpotter.au@gmail.com>
2023-07-21 10:57:34 +1000

    use logo image not text!

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Menu from "./Menu";
 
+import logo from "../images/logo.svg";
+
 import classes from "./Header.module.css";
 
 function Header() {
   return (
     <div className={classes.header}>
-      <p>snap</p>
+      <img src={logo} alt="snap logo" />
       <Menu />
     </div>
   );
