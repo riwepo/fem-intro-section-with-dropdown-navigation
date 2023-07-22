3322c9a8168381abbe0d85a201eefb6c0e8c0d49
Richard Potter <richardpotter.au@gmail.com>
2023-07-22 12:12:13 +1000

    dont need conditional render

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,30 +10,26 @@ function Navbar({ isOpen }) {
   return (
     <>
       <nav className={`${classes.nav} ${isOpen && classes.open}`}>
-        {isOpen && (
-          <>
-            <div className={classes.menuItemsContainer}>
-              <MenuItemCollapsible label="Features">
-                <FeaturesMenu />
-              </MenuItemCollapsible>
-              <MenuItemCollapsible label="Company">
-                <CompanyMenu />
-              </MenuItemCollapsible>
-              <a className={classes.anchor} href="/dummy">
-                Careers
-              </a>
-              <a className={classes.anchor} href="/dummy">
-                About
-              </a>
-            </div>
-            <button className={`${classes.button} ${classes.loginButton}`}>
-              Login
-            </button>
-            <button className={`${classes.button} ${classes.registerButton}`}>
-              Register
-            </button>
-          </>
-        )}
+        <div className={classes.menuItemsContainer}>
+          <MenuItemCollapsible label="Features">
+            <FeaturesMenu />
+          </MenuItemCollapsible>
+          <MenuItemCollapsible label="Company">
+            <CompanyMenu />
+          </MenuItemCollapsible>
+          <a className={classes.anchor} href="/dummy">
+            Careers
+          </a>
+          <a className={classes.anchor} href="/dummy">
+            About
+          </a>
+        </div>
+        <button className={`${classes.button} ${classes.loginButton}`}>
+          Login
+        </button>
+        <button className={`${classes.button} ${classes.registerButton}`}>
+          Register
+        </button>
       </nav>
       {isOpen && <div className={classes.openOverlay}></div>}
     </>
