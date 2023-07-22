343f06e14ff7fb8cf43fb8efa59ca1c4207f23f0
Richard Potter <richardpotter.au@gmail.com>
2023-07-22 12:55:40 +1000

    put footer inside main

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Header from "./components/Header";
 import Main from "./components/Main";
-import Footer from "./components/Footer";
 
 import classes from "./App.module.css";
 
@@ -11,7 +10,6 @@ function App() {
     <div className={classes.app}>
       <Header />
       <Main />
-      <Footer />
     </div>
   );
 }
